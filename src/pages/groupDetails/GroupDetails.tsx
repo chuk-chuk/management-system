4e621486a05007b497dd119e30752b43330a2e34
chuk-chuk <yulia.v.litvin@gmail.com>
2022-06-22 17:18:31 +0100

    allow to remove users, add button to add new ones

--- src/pages/groupDetails/GroupDetails.tsx
+++ src/pages/groupDetails/GroupDetails.tsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { Button } from "../../components/button/Button";
 import { GroupContext, GroupContextType } from "../../context/GroupContext";
+import { UserContext, UserContextType } from "../../context/UserContext";
 
 export function GroupDetails() {
   const { groupId } = useParams();
@@ -10,6 +11,7 @@ export function GroupDetails() {
   const { groupsList, setGroupsList } = useContext(
     GroupContext
   ) as GroupContextType;
+  const { usersList } = useContext(UserContext) as UserContextType;
 
   const groupInfo = groupsList.find((group) => group.id === groupId);
 
@@ -23,6 +25,32 @@ export function GroupDetails() {
     }
   };
 
+  const getUser = (id: string) => {
+    return usersList.find((user) => user.id === id);
+  };
+
+  const handleRemoveUser = (userIdToRemove: string) => {
+    if (groupInfo?.users && groupInfo.users.length > 0) {
+      const newGroupUserList = groupInfo?.users.filter(
+        (userId) => userId !== userIdToRemove
+      );
+
+      const newGroupList = groupsList.map((obj) => {
+        if (obj.id === groupId) {
+          return { ...obj, users: newGroupUserList };
+        }
+
+        return obj;
+      });
+
+      setGroupsList(newGroupList);
+    }
+  };
+
+  const handleAddUser = () => {
+    console.log("user added");
+  };
+
   return (
     <div className="m-24">
       {groupId && (
@@ -33,19 +61,36 @@ export function GroupDetails() {
             className="mb-8"
           />
           <div className="mb-6">
-            <p>{groupInfo?.name}</p>
-            <p>Description: {groupInfo?.description}</p>
+            <p className="mb-2">{groupInfo?.name}</p>
+            <p className="mb-6">Description: {groupInfo?.description}</p>
             {groupInfo?.users && groupInfo?.users?.length > 0 ? (
-              <div>
-                {groupInfo?.users.map((user) => (
-                  <p>{user}</p>
+              <div className="flex flex-wrap mb-8 border rounded-md">
+                {groupInfo?.users.map((userId, i) => (
+                  <div
+                    key={i}
+                    className="flex flex-col items-center justify-center w-28 h-20 m-2 rounded-md border bg-blue-400 text-white"
+                  >
+                    <div className="mb-2">
+                      {getUser(userId)?.firstName} {getUser(userId)?.lastName}
+                    </div>
+                    <Button
+                      title="x"
+                      className="py-0 mr-2 bg-red-200 text-red-500 border-red-300 self-end"
+                      onClick={() => handleRemoveUser(userId)}
+                    />
+                  </div>
                 ))}
               </div>
             ) : (
-              <p>No users yet</p>
+              <p>Group has no users, do you wanna be a first one?</p>
             )}
           </div>
           <p className="text-red-200 mb-2">{deleteMessage}</p>
+          <Button
+            title="Add group user"
+            className="bg-blue-400 text-white mr-4"
+            onClick={() => handleAddUser()}
+          />
           <Button
             title="Delete this group"
             className="bg-red-400 text-white"
