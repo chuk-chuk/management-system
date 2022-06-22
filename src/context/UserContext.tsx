0c53de660386557fbce153a95c4ad051cef6eb91
chuk-chuk <yulia.v.litvin@gmail.com>
2022-06-22 11:26:12 +0100

    useCallback to stop unnessasary re-renders

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, ReactNode, useState } from "react";
+import React, { createContext, ReactNode, useCallback, useState } from "react";
 import { v4 as uuid } from "uuid";
 import { users } from "../data";
 import { User } from "../pages/users/Users.types";
@@ -13,16 +13,19 @@ const UserContext = createContext<UserContextType | null>(null);
 const UserProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [usersList, setUsersList] = useState<User[]>(users);
 
-  const addUser = (user: User) => {
-    const newUser: User = {
-      id: uuid(),
-      firstName: user.firstName,
-      lastName: user.lastName,
-      bio: user.bio,
-      groups: [],
-    };
-    setUsersList([...usersList, newUser]);
-  };
+  const addUser = useCallback(
+    (user: User) => {
+      const newUser: User = {
+        id: uuid(),
+        firstName: user.firstName,
+        lastName: user.lastName,
+        bio: user.bio,
+        groups: [],
+      };
+      setUsersList([...usersList, newUser]);
+    },
+    [usersList]
+  );
 
   return (
     <UserContext.Provider value={{ usersList, addUser }}>
